ecfa5c90c36b07d018b475fa786f8f61232205c2
William Riley-Land <william@kun.io>
2015-09-09 09:20:14 -0500

    Clean up example-service.

--- example-service/index.js
+++ example-service/index.js
@@ -8,21 +8,21 @@ var name = pkg.name
 module.exports = function (options) {
   var seneca = this;
 
-  // seneca.options(config.seneca);
-
-  seneca.client({ host: 'localhost' });
-
   seneca.add({ role: name, cmd: 'ping' }, function (args, done) {
     done(null, { msg: 'pong' });
   });
 
   seneca.act('role:web', { use: {
-    prefix: '/api',
+    prefix: '/',
     pin: { role: name, cmd: '*' },
     map: { ping: true }
   }});
 
   express()
+    .use(function (request, response, next) {
+      request.body = {}; // stop warning message
+      next();
+    })
     .use(seneca.export('web'))
     .listen(80);
 
