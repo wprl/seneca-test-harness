ae8caab7a1b1a35d7da14942d29cb7862ff6e72f
William Riley-Land <william@kun.io>
2015-09-09 15:43:30 -0500

    Use volume to mount user files to seneca-host.

--- seneca-docker-image/index.js
+++ seneca-docker-image/index.js
@@ -3,4 +3,4 @@
 // Create a seneca instance.
 var seneca = module.exports = require('seneca')();
 // Load the seneca plugin from the inheriting image.
-seneca.use(process.env.SENECA_SERVICE_DIR);
+seneca.use(process.env.SENECA_SERVICE_PATH);
